d4f3d6abae679be1c0c4a462ec2c4deced6c9c23
Chris-Adai <christopheradai.ca@gmail.com>
2025-02-24 22:42:10 -0800

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -29,16 +29,16 @@ class LibraryDetailView(DetailView):
 # from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # from django.shortcuts import render, redirect
 
-def register_view(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("list_books")
-    else:
-        form = UserCreationForm()
-    return render(request, "relationship_app/register.html", {"form": form})
+# def register_view(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect("list_books")
+#     else:
+#         form = UserCreationForm()
+#     return render(request, "relationship_app/register.html", {"form": form})
 
 def login_view(request):
     if request.method == "POST":
