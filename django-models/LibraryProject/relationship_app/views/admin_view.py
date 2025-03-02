fa9fc9b6fd218d0311244efec1764e85955bb6ea
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-02 02:23:59 -0800

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/views/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/admin_view.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
+# def is_admin(user):
+#     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# @login_required
+# @user_passes_test(is_admin)
+# def admin_view(request):
+#     return render(request, 'admin_page.html', {'role': 'Admin'})
+
+
+# Helper function to check for 'Admin' role
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_page.html', {'role': 'Admin'})
+    return render(request, 'relationship_app/admin_view.html')
