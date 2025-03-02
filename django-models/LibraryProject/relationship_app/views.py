152394358ef4b397418fc85209286c9be2d21aae
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-02 02:48:15 -0800

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -148,6 +148,11 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
 
 
 
