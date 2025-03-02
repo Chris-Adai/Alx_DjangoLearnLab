c708a5d6e66f3b0882b6f44ce1d9e1079dc433b0
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-02 01:55:41 -0800

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
+
 
 # Create your views here.
 # from django.shortcuts import render
@@ -118,6 +120,32 @@ def delete_book(request, book_id):
         return redirect('book_list')
 
     return render(request, 'confirm_delete.html', {'book': book})
+
+#####################################################
+# from django.shortcuts import render
+
+# Helper functions to check roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 
 
