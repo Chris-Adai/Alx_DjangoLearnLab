1a9a0b37b53ca41ba253430e1906f947dad256b8
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-02 01:16:02 -0800

    Delete django-models/LibraryProject/relationship_app/librarian_view.py

--- django-models/LibraryProject/relationship_app/librarian_view.py
+++
@@ -1,7 +0,0 @@
-def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_page.html', {'role': 'Librarian'})
