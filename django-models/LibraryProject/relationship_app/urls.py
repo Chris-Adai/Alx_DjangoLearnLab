4007e0b310b06aeb1dff5ce1b9d14e23a9d9a541
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-02 00:28:36 -0800

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,21 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import list_books, LibraryDetailView
 from .views import register_view, login_view, logout_view
+# from .views.admin_view import admin_view
+# from .views.librarian_view import librarian_view
+# from .views.member_view import member_view
+from .views import admin_view
+from .views import librarian_view
+from .views import member_view
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
     # path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
 
 urlpatterns += [
@@ -19,3 +29,13 @@ urlpatterns += [
     path("logout/", auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("register/", register, name="register"),
 ]
+
+
+#######################################################
+from django.urls import path
+
+
+urlpatterns = [
+
+]
+
