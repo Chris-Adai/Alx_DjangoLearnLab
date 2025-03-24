99b5a0d009342f9bb9cbaa97672ff3a37e299fc2
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-24 11:39:16 -0700

    Add files via upload

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -13,6 +13,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
 
+
 # Initialize router
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
