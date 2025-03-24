4c9690c7b7b81fa1b7da80eab122fa6f9f32b6ac
Chris-Adai <christopheradai.ca@gmail.com>
2025-03-24 11:17:16 -0700

    Add files via upload

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,26 @@
+# # api/urls.py
+# from django.urls import path
+# from .views import BookList
+
+# urlpatterns = [
+#     path('books/', BookList.as_view(), name='book-list'),
+# ]
+
+###########################################333
+
 # api/urls.py
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# Initialize router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Simple ListAPIView
     path('books/', BookList.as_view(), name='book-list'),
+
+    # ViewSet CRUD routes
+    path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 # api/views.py
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# NEW: ViewSet for CRUD
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
