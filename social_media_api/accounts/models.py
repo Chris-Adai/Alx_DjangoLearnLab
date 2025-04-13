808802272a4e715181737464576d5a7685260ceb
Chris-Adai <christopheradai.ca@gmail.com>
2025-04-13 11:23:21 +0100

    Add files via upload

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 # Create your models here.
 class User(AbstractUser):
@@ -16,7 +17,7 @@ class User(AbstractUser):
         related_name='followers',
         blank=True
     )
-    
+
     def __str__(self):
         return self.username
     
@@ -25,7 +26,7 @@ class User(AbstractUser):
 # filepath: c:\DJANGO-PROJECTS\ALX_PROJECTS\social_media_api\accounts\models.py
 # from django.db import models
 
-User = get_user_model()
+# User = get_user_model()
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='account_posts')
